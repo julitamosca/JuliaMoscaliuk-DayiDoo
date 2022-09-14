d155bfa6b4f541b7ec2035387e95f0781dafe32e
julitamosca <juliamoscaliuk@gmail.com>
2022-09-14 20:42:13 -0300

    Update main.js
    .

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,8 +1,8 @@
 //VARIABLES
 
-let totalCompra = 0
-let agregarAlCarrito = parseInt(prompt('Ingresa el produco que deseas comprar: \n1-Camiseta \n2-Gorro \n3-Taza \n4-Llavero'))
-let seguirComprando = true
+// let totalCompra = 0
+// let agregarAlCarrito = parseInt(prompt('Ingresa el produco que deseas comprar: \n1-Camiseta \n2-Gorro \n3-Taza \n4-Llavero'))
+// let seguirComprando = true
 
 const productos = []
 const carritoDeCompras = []
@@ -36,6 +36,12 @@ class Producto {
         const container = document.getElementById('container')
         container.innerHTML += card
     }
+    agregarEvento() {
+        const btnAgregar = document.getElementById(this.id)
+        console.log(btnAgregar)
+        btnAgregar.addEventListener('click', () => agregarAlCarrito())
+
+    }
 }
 
 let camiseta1 = new Producto('01', 'Camiseta estampada', 700, './assets/camiseta01.jpg')
@@ -47,58 +53,66 @@ let llavero1 = new Producto('04', 'Llavero fantasma', 100, './assets/llavero04.j
 productos.push(camiseta1, gorro1, taza1, llavero1)
 
 console.log(productos)
+
 productos.forEach(e => {
-e.desplegarProductos()
+    e.desplegarProductos()
 })
 
+productos.forEach(e => {
+    e.agregarEvento()
+})
+
+function agregarAlCarrito(){
+    console.log(hola)
+}
 //ORDEN DE COMPRA 
 
-while (seguirComprando === true) {
-    if (agregarAlCarrito === 1) {
-        totalCompra = totalCompra + camiseta1.precio
-    } else if (agregarAlCarrito === 2) {
-        totalCompra = totalCompra + gorro1.precio
-    } else if (agregarAlCarrito === 3) {
-        totalCompra = totalCompra + taza1.precio
-    } else if (agregarAlCarrito === 4) {
-        totalCompra = totalCompra + llavero1.precio
-    } else {
-        agregarAlCarrito = parseInt(prompt('Opción incorrecta, vuelve a intentarlo: \n1-Camiseta \n2-Gorro \n3-Taza \n4-Llavero'))
-        continue
-    }
+// while (seguirComprando === true) {
+//     if (agregarAlCarrito === 1) {
+//         totalCompra = totalCompra + camiseta1.precio
+//     } else if (agregarAlCarrito === 2) {
+//         totalCompra = totalCompra + gorro1.precio
+//     } else if (agregarAlCarrito === 3) {
+//         totalCompra = totalCompra + taza1.precio
+//     } else if (agregarAlCarrito === 4) {
+//         totalCompra = totalCompra + llavero1.precio
+//     } else {
+//         agregarAlCarrito = parseInt(prompt('Opción incorrecta, vuelve a intentarlo: \n1-Camiseta \n2-Gorro \n3-Taza \n4-Llavero'))
+//         continue
+//     }
 
-    decision = parseInt(prompt('¿Quisieras agregar otro producto? 1.si - 2.no'))
-    if (decision === 1) {
-        agregarAlCarrito = parseInt(prompt('Ingresa el produco que deseas comprar: 1-Camiseta 2-Gorro 3-Taza 4-Llavero'))
-    } else if (decision === 2) {
-        seguirComprando = false
-    }
-    else {
-        agregarAlCarrito = parseInt(prompt('Opción incorrecta, vuelve a intentarlo: \n¿Quisieras agregar otro producto? 1.si - 2.no'))
-        continue
-    }
-}
+//     decision = parseInt(prompt('¿Quisieras agregar otro producto? 1.si - 2.no'))
+//     if (decision === 1) {
+//         agregarAlCarrito = parseInt(prompt('Ingresa el produco que deseas comprar: 1-Camiseta 2-Gorro 3-Taza 4-Llavero'))
+//     } else if (decision === 2) {
+//         seguirComprando = false
+//     }
+//     else {
+//         agregarAlCarrito = parseInt(prompt('Opción incorrecta, vuelve a intentarlo: \n¿Quisieras agregar otro producto? 1.si - 2.no'))
+//         continue
+//     }
+// }
 
-//FACTURACIÓN
+// //FACTURACIÓN
 
-alert('El total de su compra es: $' + totalCompra + ' Pero te espera un increible descuento.')
+// alert('El total de su compra es: $' + totalCompra + ' Pero te espera un increible descuento.')
 
-function precioFinal(monto) {
-    let descuento = 0
-    if (monto <= 500) {
-        descuento = 5
-    } else if (monto > 500 && monto <= 1500) {
-        descuento = 7
-    } else if (monto > 1500 && monto <= 2000) {
-        descuento = 10
-    } else {
-        descuento = 12
-    }
+// function precioFinal(monto) {
+//     let descuento = 0
+//     if (monto <= 500) {
+//         descuento = 5
+//     } else if (monto > 500 && monto <= 1500) {
+//         descuento = 7
+//     } else if (monto > 1500 && monto <= 2000) {
+//         descuento = 10
+//     } else {
+//         descuento = 12
+//     }
 
-    let montoDescuento = monto * (descuento / 100)
-    monto = monto - montoDescuento
-    return monto
-}
+//     let montoDescuento = monto * (descuento / 100)
+//     monto = monto - montoDescuento
+//     return monto
+// }
 
-let precioUltimo = precioFinal(totalCompra)
-alert('El precio final de tu compra es: $' + precioUltimo)
+// let precioUltimo = precioFinal(totalCompra)
+// alert('El precio final de tu compra es: $' + precioUltimo)
